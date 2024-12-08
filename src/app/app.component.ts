b1dd9070b3f076f7e6453d82eb2d70cc5970c4f7
jefferyrutherford <jefferyrutherford@outlook.com>
2024-12-08 16:26:03 -0600

    Initial Set Up Of Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TournamentService} from "../services/tournament.service";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Social-Gaming-UI';
+  posts:any;
+
+  constructor(private tournamentService : TournamentService) {
+    console.log(tournamentService.getHeroes());
+  }
+
+
+  ngOnInit() {
+    this.tournamentService.getPosts()
+      .subscribe(response => {
+        this.posts = response;
+      });
+
+
+    console.log(this.posts);
+  }
 }
---
+++ src/services/tournament.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TournamentService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  //private url = 'http://jsonplaceholder.typicode.com/posts';
+  private url = 'http://localhost:8080/tournament';
+
+
+
+
+  getPosts(){
+
+    return this.httpClient.get(this.url);
+
+  }
+
+
+  getHeroes(): ({ name: string; age: number } | { name: string; age: number } | { name: string; age: number })[] {
+    const jsonArray = [
+      { "name": "John", "age": 30 },
+      { "name": "Mary", "age": 25 },
+      { "name": "Peter", "age": 35 }
+    ];
+
+    const tournaments = of(jsonArray)
+    return jsonArray;
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+}
