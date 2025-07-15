16563f89372246333f13ab0de175966cc767ecae
Jeffery Rutherford <jefferyrutherford@outlook.com>
2025-07-14 20:21:33 -0600

    Fixed an issue in which jpgs in the assets folder was not building into the app.
    **Dont include any more assets going forward** Just wanted to create the folder here.

--- src/app/components/my-user-page/my-user-page.component.ts
+++ src/app/components/my-user-page/my-user-page.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {ApiService} from '../../service/api/api.service';
 
 @Component({
   selector: 'app-my-user-page',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './my-user-page.component.html',
   styleUrl: './my-user-page.component.css'
 })
-export class MyUserPageComponent {
-
+export class MyUserPageComponent implements OnInit{
+  users: any
+  constructor(private apiService: ApiService) {
+  }
+  ngOnInit() {
+    this.apiService.get('admin/getUsers').subscribe(response => {
+      this.users = response;
+    });
+  }
 }
