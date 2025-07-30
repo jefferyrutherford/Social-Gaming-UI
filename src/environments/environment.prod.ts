335445be65eceb9d7598d87ddd9b2fc9be62815e
Jeffery Rutherford <jefferyrutherford@outlook.com>
2025-07-29 22:15:51 -0600

    Removing commented out code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,8 @@ export const environment = {
   production: true,
   cognito: {
     clientId: '7nb439s4gah8m4j7frd0diqk0i',
-    // domain: 'testerino.auth.us-east-2.amazoncognito.com',
     domain: 'socialgaming.auth.us-east-2.amazoncognito.com',
-    redirectUri: 'http://localhost:4200'//,
-    //logoutUri: 'http://localhost:4200'
+    redirectUri: 'http://localhost:4200'
   },
   imageBaseUrl: '/assets/playerCard/'
 };
